d32b692c393eb4c9d0e571f3280bd71adfa22537
dk-denno <dennisveer27@gmail.com>
2018-07-19 22:15:58 +0300

    added player two if function

--- js/dice.js
+++ js/dice.js
@@ -8,9 +8,9 @@ $(document).ready(function(){
     var rolled=Math.floor(Math.random() * (max - min + 1));
       if (rolled===0) {
       alert(rolled +"," + "YOU ROLLED A ZERO...ROLL AGAIN")
-    }else if (rolled===2) {
-          $("#player1").hide(400);
-          $("#player2").show(1200);
+    }else if (rolled===1) {
+          // $("#player1").hide(400);
+          // $("#player2").show(1200);
           alert("player 1, You rolled a 1 niccur your turn is over pass the mouse")
     }else {
         $("p#roll1").prepend("<li>" + rolled + "</li>");
@@ -20,19 +20,39 @@ $(document).ready(function(){
         $('#P1rolls').text(P1rollCounter)
         $("#score1").text(player1Score);
     }
-//      var score=rolled++rolled;
+//      var score=rolled++rolled
 //    alert("your score is " + score);
 
   });
 
 
 });
+
 $(document).ready(function(){
   $("#roller").click(function(event){
     var min=1;
-    var max=10;
+    var max=6;
     var roller=Math.floor(Math.random() * (max - min + 1));
     alert(roller);
+    if (roller===0) {
+      alert(roller +"," + "YOU ROLLED A ZERO...ROLL AGAIN")
+
+    }else if (roller===1) {
+      // $("#player1").show(400);
+      // $("#player2").hide(1200);
+      alert("player 1, You rolled a 1 niccur your turn is over pass the mouse")
+
+
+    }
+
+
+
+
+
+
+
+
+
   });
 
 });
