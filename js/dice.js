f1621698ac526f6fe0076f7c70fcbe9eaf6db97c
dk-denno <dennisveer27@gmail.com>
2018-07-19 23:32:41 +0300

    added the player 2 interface

--- js/dice.js
+++ js/dice.js
@@ -1,16 +1,17 @@
+// player 1 interface.
 var P1rollCounter=0;
 var player1Score=0;
 $(document).ready(function(){
-  $("#roll").click(function(event){
+  $("#roll1").click(function(event){
     var min=1;
     var max=6;
-
+//math function to display random numbers
     var rolled=Math.floor(Math.random() * (max - min + 1));
       if (rolled===0) {
       alert(rolled +"," + "YOU ROLLED A ZERO...ROLL AGAIN")
     }else if (rolled===1) {
-          // $("#player1").hide(400);
-          // $("#player2").show(1200);
+           $("#player1").hide(400);
+           $("#player2").show(1200);
           alert("player 1, You rolled a 1 niccur your turn is over pass the mouse")
     }else {
         $("p#roll1").prepend("<li>" + rolled + "</li>");
@@ -24,35 +25,35 @@ $(document).ready(function(){
 //    alert("your score is " + score);
 
   });
-
-
 });
 
+  var player2Score=0;
+  var p2rollCounter=0;
 $(document).ready(function(){
-  $("#roller").click(function(event){
-    var min=1;
-    var max=6;
-    var roller=Math.floor(Math.random() * (max - min + 1));
-    alert(roller);
-    if (roller===0) {
-      alert(roller +"," + "YOU ROLLED A ZERO...ROLL AGAIN")
+  $("#roll2").click(function(event){
+    var less=1;
+    var top= 6;
 
-    }else if (roller===1) {
-      // $("#player1").show(400);
-      // $("#player2").hide(1200);
+    var rolled1=Math.floor(Math.random() * (top - less + 1));
+//confirmational test of the .random()function    alert(rolled1);
+    if (rolled1===0) {
+      alert(rolled1 +"," + "YOU ROLLED A ZERO...ROLL AGAIN")
+
+    }else if (rolled1===1) {
+       $("#player1").show(1200);
+      $("#player2").hide(400);
       alert("player 1, You rolled a 1 niccur your turn is over pass the mouse")
 
+    }else {
+      $("p#roll2").prepend("<li>" + rolled1 + "</li>")
 
     }
-
-
-
-
-
-
-
+    //score=current roll + 0/previous roll/sum of previous rolls
+    player2Score=player2Score+parseInt(rolled1);
+    p2rollCounter=p2rollCounter+=1;
+    $('#p2rolls').text(p2rollCounter);
+    $("#score2").text(player2Score);
 
 
   });
-
 });
