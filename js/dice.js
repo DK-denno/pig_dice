66aca4d1ab360e439c223fa0ebb1cc476a3e9c02
dk-denno <dennisveer27@gmail.com>
2018-07-19 12:49:55 +0300

    created a roll button for the second player

--- js/dice.js
+++ js/dice.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
   $("#roll").click(function(event){
-    var min=0;
+    var min=1;
     var max=10;
     var rolled=Math.floor(Math.random() * (max - min + 1));
-    alert(rolled);
+      if (rolled===0) {
+      alert(rolled +"," + "YOU ROLLED A ZERO...ROLL AGAIN")
+    }else if (rolled===1) {
+      alert("player 1, You rolled a 1 niccur your turn is over pass the mouse")
+    }else {
+        alert(rolled);
+    }
+//      var score=rolled++rolled;
+//    alert("your score is " + score);
+
   });
+
+
+});
+$(document).ready(function(){
+  $("#roller").click(function(event){
+    var min=1;
+    var max=10;
+    var roller=Math.floor(Math.random() * (max - min + 1));
+    alert(roller);
+  });
+
 });
